624a9193a2f8cd4225a6800806cbc2ee28147331
saabdoli <saabdoli@gmail.com>
2015-10-28 00:11:32 +0000

    Added extra information to Member Schema
    In this commit, I went through and added all missing info from the current
    Member signup form to the schema for Members. I decided to use nested documents
    to store the multiple groups.

--- app/models/member.server.model.js
+++ app/models/member.server.model.js
@@ -26,6 +26,10 @@ var MemberSchema = new Schema({
 		type: String,
 		trim: true
 	},
+	member: {
+		type: Boolean,
+		default: true,
+	},
 	phone: {
 		type: String,
 		trim: true,
@@ -38,11 +42,23 @@ var MemberSchema = new Schema({
 		default: '',
 		validate: [/^\s*$|.+\@.+\..+/, 'Please provide a valid email address']
 	},
+	address: {             // Should address be composed of subdocuments?
+		type: String,
+		default: '',
+	},
 	identity: {
 		type: String,
 		trim: true,
 		default: '',
 		required: 'Please choose an option for "Who are you ?"'
+	},
+	shirtSize: {
+		type: String,
+		required: 'Please choose a shirt size'
+	},
+	talent: {
+		type: String,
+		required: 'Please provide a talent/passion.'
 	},
 	affiliation: {
 		type: String,
@@ -50,14 +66,25 @@ var MemberSchema = new Schema({
 		default: '',
 		required: 'Please fill in the affiliation'
 	},
-	member: {
-		type: Boolean,
-		default: false,
-	},
 	placeOfWorship: {
 		type: String,
 		trim: true,
 		default: '',
+	},
+	communityNetworks: {
+		network1: {type: String},
+		network2: {type: String},
+		network3: {type: String}
+	},
+	extraGroups: {                            //is this the best way to store these groups
+		group1: {type: String},
+		group2: {type: String},
+		group3: {type: String},
+	},
+	otherNetworks: {
+		otherNetwork1: {type: String},
+		otherNetwork2: {type: String},
+		otherNetwork3: {type: String},
 	},
 	created: {
 		type: Date,
