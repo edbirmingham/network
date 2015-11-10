633ba459dd0d94dd631296809b9bba01f01fdfca
Anthony Crumley <anthony.crumley@gmail.com>
2015-11-10 21:06:56 +0000

    Build for disambiguating participant search

--- public/dist/application.js
+++ public/dist/application.js
@@ -813,12 +813,20 @@ angular.module('participants').controller('ParticipantsController', ['$scope', '
 //Participants service used to communicate Participants REST endpoints
 angular.module('participants').factory('Participants', ['$resource',
 	function($resource) {
-		return $resource('participants/:participantId', { participantId: '@_id'
+		var Participant = $resource('participants/:participantId', { participantId: '@_id'
 		}, {
 			update: {
 				method: 'PUT'
 			}
 		});
+		
+		angular.extend(Participant.prototype, {
+			listName: function() {
+				return this.displayName + ' (' + this.phone + ')';
+			}
+		});
+		
+		return Participant;
 	}
 ]);
 'use strict';
