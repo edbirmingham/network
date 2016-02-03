9f90ae6c7b80980eb47e2b0c3ea9efc62dbdc5bf
saabdoli <saabdoli@gmail.com>
2016-02-03 15:22:49 +0000

    Added base formatting for Connector Home Page, connector methods
    In this commit I added formatting to the connector home page
    that can encase the queried info for each connector when they log in,
    I also added some stuff to the controller for the home page.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,5 +6,20 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 		
+<<<<<<< HEAD
+=======
+		
+		
+	}
+]);
+
+angular.module('connector').controller('ConnController', ['$scope', 'Authentication', 'Actions', 'Members', 'Users',
+	function($scope, Authentication, Actions, Members, Users) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+		
+		
+		
+>>>>>>> Added base formatting for Connector Home Page, connector methods
 	}
 ]);
