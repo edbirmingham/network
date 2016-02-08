70215b06c7e6e2503e091029fb8f9c972afee61f
saabdoli <saabdoli@gmail.com>
2016-02-08 23:31:25 +0000

    Make Dashboard only visible to Connectors
    Make Dashboard section only visible to connector by testing for
    'connector' membership in roles array

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,10 +5,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication, Actions, Users) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+<<<<<<< HEAD
 
 		$scope.currentUser = $scope.authentication.user;
 		
 		$scope.showConnectorHome = false;
+=======
+>>>>>>> Make Dashboard only visible to Connectors
 		
 	}
 ]);
--- public/modules/users/controllers/users.client.controller.js
+++ public/modules/users/controllers/users.client.controller.js
@@ -4,10 +4,15 @@
 angular.module('users').controller('UsersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Users',
 	function($scope, $stateParams, $location, Authentication, Users) {
 		$scope.authentication = Authentication;
+<<<<<<< HEAD
 		$scope.user = {};
 		$scope.isconnector = null;
 		var currentUser = $scope.authentication.user;
 		$scope.isconnector = null;
+=======
+		var currentUser = $scope.authentication.user;
+		
+>>>>>>> Make Dashboard only visible to Connectors
 
 		// Create new User
 		$scope.create = function() {
@@ -71,15 +76,12 @@ angular.module('users').controller('UsersController', ['$scope', '$stateParams',
 		};
 		
 		$scope.isConnector = function() {
-			if($scope.user.roles.indexOf('connector') > -1) {
-				$scope.isconnector = true;
+			if(currentUser.roles.indexOf('connector') > -1) {
+				return true;
 			}
 			else {
-				$scope.isconnector = false;
+				return false;
 			}
-			
-			if (document.getElementById('isconnector') !== null)
-				document.getElementById('isconnector').checked = $scope.isconnector;
 		};
 		
 		$scope.setConnector = function() {
