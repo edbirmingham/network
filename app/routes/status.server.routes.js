fcca57bbb8db15009b0f230f11af942b995ea2bb
Anthony Crumley <anthony.crumley@gmail.com>
2016-06-21 20:51:30 -0500

    Removed MEAN.JS code making room for Rails

--- app/routes/status.server.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-	var status = require('../../app/controllers/status.server.controller');
-
-    // Participants Routes
-    app.route('/status')
-    	.get(status.check);
-};
