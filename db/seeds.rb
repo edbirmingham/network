65583fc940f19af59ed1c8dd4324eda5f7d990a8
saabdoli <mauveisfine@gmail.com>
2016-06-29 14:44:26 +0000

    Enable recording of user during creation of records

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Location.create(name: 'Tuggle Elementary')
-Location.create(name: 'Carver High School')
-Location.create(name: 'Hudson K-8')
-Location.create(name: 'Ramsay High School')
+user = User.create(email: 'jane.doe@example.com', password: 'password')
+User.create(email: 'john.doe@example.com', password: 'password')
+
+Location.create(name: 'Tuggle Elementary', user_id: user.id)
+Location.create(name: 'Carver High School', user_id: user.id)
+Location.create(name: 'Hudson K-8', user_id: user.id)
+Location.create(name: 'Ramsay High School', user_id: user.id)
 
 Member.create(
   first_name: 'Victoria', 
   last_name: 'Hollis',
   email: 'victoria@example.com',
-  phone: '205 999-9999'
+  phone: '205 999-9999',
+  user_id: user.id
 )
 Member.create(
   first_name: 'Chris', 
   last_name: 'Collins',
   email: 'chris@example.com',
-  phone: '205 999-9999'
+  phone: '205 999-9999',
+  user_id: user.id
 )
 Member.create(
   first_name: 'Andrew', 
   last_name: 'Sellers',
   email: 'andrew@example.com',
-  phone: '205 999-9999'
+  phone: '205 999-9999',
+  user_id: user.id
 )
 Member.create(
   first_name: 'Sean', 
   last_name: 'Abdoli',
   email: 'sean@example.com',
-  phone: '205 999-9999'
+  phone: '205 999-9999',
+  user_id: user.id
 )
 
-Program.create(name: 'College 101')
-Program.create(name: 'Raise Up Initiatives')
-Program.create(name: 'Educator Round Table')
-Program.create(name: 'Connector Table Meeting')
-Program.create(name: 'Core Team Meeting')
+Program.create(name: 'College 101', user_id: user.id)
+Program.create(name: 'Raise Up Initiatives', user_id: user.id)
+Program.create(name: 'Educator Round Table', user_id: user.id)
+Program.create(name: 'Connector Table Meeting', user_id: user.id)
+Program.create(name: 'Core Team Meeting', user_id: user.id)
 
-User.create(email: 'jane.doe@example.com', password: 'password')
-User.create(email: 'john.doe@example.com', password: 'password')
 
 Location.all.each do |location|
   Program.all.each do |program|
@@ -51,7 +56,8 @@ Location.all.each do |location|
       name: "#{location.name} #{program.name}",
       program: program,
       location: location,
-      scheduled_at: rand(14).days.from_now
+      scheduled_at: rand(14).days.from_now,
+      user_id: user.id
     )
   end
 end
