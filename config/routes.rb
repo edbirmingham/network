71076990dc5545e6d9f3b8043d8baaf20ba0e371
Tom Miller <tmiller@eab.com>
2016-07-05 21:54:49 -0500

    Restrict availble actions for users
    For security reasons

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   devise_for :users
-  resources :users
+  resources :users, only: [:index, :destroy]
   
   get 'welcome/index'
 
