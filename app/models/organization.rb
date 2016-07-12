c37fa74e00bcd045ebd11faefd2d8abea1068ad7
Zac Williams <zac@zacwilliams.com>
2016-07-12 16:37:22 -0500

    Added associations for users/organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ActiveRecord::Base
   validates :name, presence: true
+
+  belongs_to :created_by, class_name: "User", foreign_key: :created_by_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :created_organizations, class_name: "Organization", foreign_key: :created_by_id
 end
