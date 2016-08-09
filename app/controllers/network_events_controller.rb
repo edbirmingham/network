86386b071fc1448f75bb1dd0385b18ad2ed0dc8a
saabdoli <mauveisfine@gmail.com>
2016-08-09 15:13:32 +0000

    Add ability to sort columns on Event page

--- app/controllers/network_events_controller.rb
+++ app/controllers/network_events_controller.rb
@@ -7,11 +7,11 @@ class NetworkEventsController < ApplicationController
   def index
     if params[:start_date].present? && params[:end_date].present?
       @network_events = NetworkEvent.in_date_range( params[:start_date], params[:end_date])
-        .includes(:program, :location, :organization)
+        .includes(:program, :location)
         .order(sort_column + " " + sort_direction)
     else
       @network_events = NetworkEvent.default_date_range
-        .includes(:program, :location, :organization)
+        .includes(:program, :location)
         .order(sort_column + " " + sort_direction)
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   
   def sortable(column, title=nil)
     title = column.titleize
-    join_column = column + 's.name'
+    join_column = column.pluralize + '.name'
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     # join columns dont change sort directions because, 
     direction = (column == sort_column or join_column == sort_column) && sort_direction == "asc" ? "desc" : "asc"
