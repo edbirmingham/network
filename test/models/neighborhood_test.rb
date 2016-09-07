c8f85a6786482fde33ff8875a75aeaf3712ed4ff
Jamall Harris <jamallharris@absinf.com>
2016-09-07 14:19:32 -0500

    added testing to several models

--- test/models/neighborhood_test.rb
+++ test/models/neighborhood_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class NeighborhoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Neighborhood should not save with name" do
+    neighborhood = Neighborhood.new
+    assert_not neighborhood.save
+  end
+  test "Neighborhood should save with name" do
+    neighborhood = Neighborhood.new(name:"test neighborhood")
+    assert neighborhood.save
+  end
 end
