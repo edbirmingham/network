0e7965624e25fe243e4c7e32cebeb183a29b3da8
Tom Miller <jackerran@gmail.com>
2017-04-11 23:04:44 -0500

    cleanup whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,12 @@ module ApplicationHelper
       link_to number_to_phone(number), "tel:#{number}"
     end
   end
-  
+
   def sortable(column, title=nil)
     title = column.titleize
     join_column = column.pluralize + '.name'
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    # join columns dont change sort directions because, 
+    # join columns dont change sort directions because,
     direction = (column == sort_column or join_column == sort_column) && sort_direction == "asc" ? "desc" : "asc"
     link_to title, params.permit!.merge({:sort => column, :direction => direction}), {:class => css_class}
   end
