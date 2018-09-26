0f041a156e8be6e07533b1c920d8cf7c929a3da0
Anthony Crumley <anthony.crumley@gmail.com>
2018-09-26 01:03:29 +0000

    Add missing pagination for survey recipients
    On the survey show page, only the first page of recipients where being
    retrieved for each collector.  In the process of adding pagination, I
    found out how to retrieve recipient names and added that to the
    recipients listing to make it easier to use.

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,16 @@
 module SurveysHelper
+  def recipient_name(recipient)
+    if recipient[:last_name].present? || recipient[:first_name].present?
+      name = [recipient[:last_name], recipient[:first_name]].join(', ')
+    else
+      name = ""
+    end
+    
+    if recipient[:custom_fields].present?
+      member_id = recipient[:custom_fields][:"1"]
+      link_to name, member_path(member_id) 
+    else
+      name
+    end
+  end
 end
